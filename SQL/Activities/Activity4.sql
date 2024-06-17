a40d58ed314a329840f39c913f697d09344c4ec2
babyp31 <141715704+babyp31@users.noreply.github.com>
2024-06-17 11:52:31 +0530

    Add files via upload

---
+++ SQL/Activities/Activity4.sql
@@ -0,0 +1,10 @@
+SELECT salesman_id, salesman_city FROM salesman;
+--------------------------------------------------Activity4
+-- Add the grade column
+ALTER TABLE salesman ADD grade int;
+
+-- Update the values in the grade column
+UPDATE salesman SET grade=100;
+
+-- Display data
+SELECT * FROM salesman;
